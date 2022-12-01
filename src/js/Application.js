89fac5a123b806e3bb8cd1ab1705892b09be616d
Stanislava Pavlova <stanislavap2002@gmail.com>
2022-12-01 22:42:56 +0200

    task

--- src/js/Application.js
+++ src/js/Application.js
@@ -1,21 +1,28 @@
-import EventEmitter from "eventemitter3";
+import EventEmitter from 'eventemitter3';
 
 export default class Application extends EventEmitter {
   static get events() {
     return {
-      READY: "ready",
+      READY: 'ready',
     };
   }
 
   constructor() {
     super();
     this.emojis = [];
-    this.banana = "🍌";
+    this.banana = '🍌';
     this.emit(Application.events.READY);
   }
   setEmojis(emojis) {
     this.emojis = emojis;
+    // display the elements
+    let paragraph = (document.createElement('p').textContent = this.emojis);
+    document.getElementById('emojis').appendChild(paragraph);
   }
 
-  addBananas() {}
+  addBananas() {
+    // add a bannana to each array element (monkey)
+    this.emojis = this.emojis.map((m) => m + this.banana);
+    document.getElementById('emojis').textContent = '';
+  }
 }
